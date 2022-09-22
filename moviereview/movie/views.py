41128ac67fd5c90aaea62cfe9ef7e0dbcd2116fd
dfbrasil <danieldfb@gmail.com>
2022-09-22 09:45:33 -0300

    movie2

--- moviereview/movie/views.py
+++ moviereview/movie/views.py
@@ -1,10 +1,17 @@
 from re import search
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm':searchTerm})
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html',
+        {'searchTerm':searchTerm, 'movies': movies}
+)
 
 def about(request):
     return HttpResponse('<h1>Sobre</h1>')
@@ -12,4 +19,3 @@ def about(request):
 def signup(request):
     email = request.GET.get('email')
     return render(request, 'signup.html', {'email':email})
-
